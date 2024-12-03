b8c85eac855c2ddb3073dc050cb839f957daf15f
Paval-from-Belarus <star-war-sandakan@mail.ru>
2024-12-03 11:22:54 +0300

    prepare mono repository

--- blue_worker/src/main.rs
+++ blue_worker/src/main.rs
@@ -1,20 +1,25 @@
 #![no_std]
 #![no_main]
 
-mod worker;
 mod shared_data;
+mod worker;
 
+// use esp32_nimble::{BLEDevice, BLEScan};
 use esp_backtrace as _;
 use esp_hal::prelude::*;
+use esp_idf_hal::task;
 use log::info;
 
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
+// use esp_idf_sys as _;
 
 extern crate alloc;
 
 #[esp_hal_embassy::main]
 async fn main(spawner: Spawner) {
+    // esp_idf_sys::link_patches();
+
     let peripherals = esp_hal::init({
         let mut config = esp_hal::Config::default();
         config.cpu_clock = CpuClock::max();
@@ -28,7 +33,6 @@ async fn main(spawner: Spawner) {
     let timer0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1);
     esp_hal_embassy::init(timer0.timer0);
 
-
     let timer1 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
     let _init = esp_wifi::init(
         timer1.timer0,
@@ -37,12 +41,27 @@ async fn main(spawner: Spawner) {
     )
     .unwrap();
 
-    // TODO: Spawn some tasks
+    // let ble_device = BLEDevice::take();
+    //
+    // let mut ble_scan = BLEScan::new();
+
+    task::block_on(async {
+        // ble_scan.active_scan(true).interval(100).window(99);
+        //
+        // ble_scan
+        //     .start(ble_device, 5000, |device, data| {
+        //         log::info!("Advertised Device: ({:?}, {:?})", device, data);
+        //         None::<()>
+        //     })
+        //     .await?;
+    });
+
+    log::info!("Scan finished");
+
     let _ = spawner;
 
     loop {
         info!("Hello world!");
         Timer::after(Duration::from_secs(1)).await;
     }
-
 }
